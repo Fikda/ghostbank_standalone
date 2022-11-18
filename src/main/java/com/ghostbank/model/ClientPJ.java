da1ed929c524778cac88c88568fc907895ec29b7
FABIANA  IKEDA <fabianaikeda@yahoo.com.br>
2022-11-18 14:58:07 -0300

    model pj

--- src/main/java/com/ghostbank/model/ClientPJ.java
+++ src/main/java/com/ghostbank/model/ClientPJ.java
@@ -1,6 +1,36 @@
 package com.ghostbank.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import java.util.Objects;
+
+@Getter
+@Setter
+@Entity
 public class ClientPJ extends Client{
 
+ private String cnpj;
 
+    public ClientPJ() {
+    }
+
+    public ClientPJ(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        ClientPJ clientPJ = (ClientPJ) o;
+        return Objects.equals(cnpj, clientPJ.cnpj);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), cnpj);
+    }
 }
